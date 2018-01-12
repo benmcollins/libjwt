17076b6bccca2c9b402365b70d3196dac2711289
Ben Collins <ben@cyphre.com>
2018-01-12 08:28:08 -0500

    Move LibJWT to MPL-2.0 license from LGPLv3.
    Approved by Nicolas Mora for jwt-gnutls.c as well.
    
    Signed-off-by: Ben Collins <ben@cyphre.com>

--- libjwt/jwt.c
+++ libjwt/jwt.c
@@ -1,19 +1,9 @@
-/* Copyright (C) 2015-2017 Ben Collins <ben@cyphre.com>
+/* Copyright (C) 2015-2018 Ben Collins <ben@cyphre.com>
    This file is part of the JWT C Library
 
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 3 of the License, or (at your option) any later version.
-
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public
-   License along with the JWT Library; if not, see
-   <http://www.gnu.org/licenses/>.  */
+   This Source Code Form is subject to the terms of the Mozilla Public
+   License, v. 2.0. If a copy of the MPL was not distributed with this
+   file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include <stdlib.h>
 #include <string.h>
