4bb0c0196f7dc4df36a26526836c91de660e81ea
Ben Collins <ben@cyphre.com>
2019-05-30 00:48:54 +0000

    v1.10.2
    Signed-off-by: Ben Collins <ben@cyphre.com>

--- navtreedata.js
+++ navtreedata.js
@@ -1,6 +1,6 @@
 var NAVTREE =
 [
-  [ "libjwt-1.10.1", "index.html", [
+  [ "libjwt-1.10.2", "index.html", [
     [ "Deprecated List", "deprecated.html", null ],
     [ "Modules", "modules.html", "modules" ],
     [ "Files", null, [
