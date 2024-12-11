6355e5783939bf58d710470b902c9a48dbd8c4e7
Ben Collins <bcollins@maclara-llc.com>
2024-12-11 15:08:42 -0500

    pem_to_jwk: Add a warning about the lack of error checking
    Signed-off-by: Ben Collins <bcollins@maclara-llc.com>

--- tests/keys/jwks/pem_to_jwk.c
+++ tests/keys/jwks/pem_to_jwk.c
@@ -6,6 +6,8 @@
    License, v. 2.0. If a copy of the MPL was not distributed with this
    file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+/* XXX BIG FAT WARNING: There's not much error checking here. */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
