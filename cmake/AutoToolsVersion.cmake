102f223b3575d4c68c59ade1d41e58d9e9ec686b
Ben Collins <bcollins@maclara-llc.com>
2024-12-17 10:54:29 -0500

    build: Work out some inconsistencies in cmake vs autotools
    Signed-off-by: Ben Collins <bcollins@maclara-llc.com>

--- cmake/AutoToolsVersion.cmake
+++ cmake/AutoToolsVersion.cmake
@@ -2,7 +2,7 @@
 FILE(STRINGS "${CMAKE_SOURCE_DIR}/configure.ac" configure REGEX "m4_define")
 foreach (line ${configure})
     foreach (var major_version minor_version micro_version
-		so_current so_revision so_age)
+		so_current so_revision so_age libjwt_project)
 	if (NOT ${var} AND line MATCHES "m4_define\\(\\[${var}\\],.*\\[(.*)\\]\\)")
             set(${var} "${CMAKE_MATCH_1}")
             break()
@@ -11,6 +11,7 @@ foreach (line ${configure})
 endforeach()
 
 set(LIBJWT_VERSION "${major_version}.${minor_version}.${micro_version}")
+set(LIBJWT_PROJECT "${libjwt_project}")
 
 # WWLTD (What would libtool do)
 math(EXPR SO_MAJOR "${so_current} - ${so_age}")
