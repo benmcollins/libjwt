579f81bf17ad17de199c4d186d527e4a669319be
Ben Collins <bcollins@ubuntu.com>
2025-01-02 12:33:20 +0000

    Correct issue with base64.h needing our export defs
    Signed-off-by: Ben Collins <bcollins@ubuntu.com>

--- libjwt/base64.h
+++ libjwt/base64.h
@@ -1,6 +1,9 @@
 #ifndef BASE64_H
 #define BASE64_H
 
+#include <jwt.h>
+#include "jwt-private.h"
+
 #define BASE64_ENCODE_OUT_SIZE(s) ((unsigned int)((((s) + 2) / 3) * 4 + 1))
 #define BASE64_DECODE_OUT_SIZE(s) ((unsigned int)(((s) / 4) * 3))
 
@@ -8,13 +11,15 @@
  * out is null-terminated encode string.
  * return values is out length, exclusive terminating `\0'
  */
-JWT_NO_EXPORT unsigned int
-base64_encode(const unsigned char *in, unsigned int inlen, char *out);
+JWT_NO_EXPORT
+extern unsigned int base64_encode(const unsigned char *in, unsigned int inlen,
+				  char *out);
 
 /*
  * return values is out length
  */
-JWT_NO_EXPORT unsigned int
-base64_decode(const char *in, unsigned int inlen, unsigned char *out);
+JWT_NO_EXPORT
+extern unsigned int base64_decode(const char *in, unsigned int inlen,
+				  unsigned char *out);
 
 #endif /* BASE64_H */
